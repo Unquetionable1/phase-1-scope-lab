3b2d4a174ccf8381d45a1735d12fef4406d318fc
Edmound <eddnjoori@gmail.com>
2024-03-25 12:40:37 +0300

    Updates the index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName="bob";
+
+function upperCaseCustomerName(){
+    customerName='BOB';
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+    return bestCustomer;
+}
+
+
+const leastFavoriteCustomer='Assignment to constant variable.';
+   
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='not bob';
+    return leastFavoriteCustomer;
+}
